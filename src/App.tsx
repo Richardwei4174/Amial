5ee8888b81b626905a4528ee4f63c0cb9c9c9a9d
Richard Wei <Richardwei4174@gmail.com>
2026-05-12 11:49:19 -0400

    Initialize Amial V1 project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <h1 className="text-4xl font-bold p-10">
+        Amial V1
+      </h1>
+    </div>
+  );
+}
+
+export default App;
